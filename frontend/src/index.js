401d041ccf1a807d60c1727606480c2ec4ff6dd4
leningael <lrosas@dealergeek.com>
2023-05-07 12:29:36 -0600

    feat: App.js routes changed to support the new versions with 'element' and index.js changed to use createRoot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
-//Paul Hernandez y Vianey Martinez 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import store from './store'
 import './index.css';
@@ -8,14 +7,10 @@ import './bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
