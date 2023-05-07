fe4d53aefedf59bd6e90fc445ab7fe90814e6530
leningael <lrosas@dealergeek.com>
2023-05-07 12:34:37 -0600

    fix: Paginate component changed to solve the error of include a '?' character in a manually specified `to.pathname` field

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,29 +1,34 @@
-//Paul Hernandez 
-import React from 'react'
-import { Pagination } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+//Paul Hernandez
+import React from "react";
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Paginate({ pages, page, keyword = '', isAdmin = false }) {
-    if (keyword) {
-        keyword = keyword.split('?keyword=')[1].split('&')[0]
-    }
+function Paginate({ pages, page, keyword = "", isAdmin = false }) {
+  if (keyword) {
+    keyword = keyword.split("?keyword=")[1].split("&")[0];
+  }
 
-    return (pages > 1 && (/*numero de pestañas de productos*/
-        <Pagination>
-            {[...Array(pages).keys()].map((x) => (
-                <LinkContainer
-                    key={x + 1}
-                    to={!isAdmin ?
-                        `/?keyword=${keyword}&page=${x + 1}`
-                        : `/admin/productlist/?keyword=${keyword}&page=${x + 1}`
-                    }
-                >
-                    <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
-                </LinkContainer>
-            ))}
-        </Pagination>
+  return (
+    pages > 1 /*numero de pestañas de productos*/ && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={
+              !isAdmin
+                ? { pathname: "/", search: `?keyword=${keyword}&page=${x + 1}` }
+                : {
+                    pathname: "/admin/productlist",
+                    search: `?keyword=${keyword}&page=${x + 1}`,
+                  }
+            }
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
+      </Pagination>
     )
-    )
+  );
 }
 
-export default Paginate
+export default Paginate;
